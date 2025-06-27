f37910919e53f97416220b4fb3aa7ca38580fd9b
chrisdwells <c.d.wells@leeds.ac.uk>
2025-06-27 13:04:56 +0100

    setting up spinup

---
+++ frida_clim_processing/scripts/0X_historical_parameters.py
@@ -0,0 +1,17 @@
+
+
+
+
+Fixed T, CO2 (will be varied in historical ensemble):
+Forest.CO2 tree net primary production parameter[1]
+Forest.STA maximum aboveground biomass per area parameter[1]
+Forest.STA squared maximum aboveground biomass per area parameter[1]
+Forest.STA squared tree net primary production parameter[1]
+Forest.STA tree net primary production parameter[1]
+Grass.CO2 grass net primary production parameter[1]
+Grass.STA grass net primary production parameter[1]
+Grass.STA squared grass net primary production parameter[1]
+soil carbon decay.temp response[1]
+
+No energy crops (will be varied in historical ensemble):
+Crop.harvest index for energy crops[1]
